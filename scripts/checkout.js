a7c59f5c8c170ae9ab9d08d1d0079bbac61242db
MohitCode17 <mohit.codes17@gmail.com>
2024-04-13 22:20:45 +0530

    Fixing radio button select

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -63,7 +63,7 @@ cart.forEach((cartItem) => {
                 type="radio"
                 checked
                 class="delivery-option-input"
-                name="delivery-option-1"
+                name="delivery-option-${matchingProduct.id}"
                 />
                 <div>
                 <div class="delivery-option-date">Tuesday, June 21</div>
@@ -74,7 +74,7 @@ cart.forEach((cartItem) => {
                 <input
                 type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1"
+                name="delivery-option-${matchingProduct.id}"
                 />
                 <div>
                 <div class="delivery-option-date">Wednesday, June 15</div>
@@ -85,7 +85,7 @@ cart.forEach((cartItem) => {
                 <input
                 type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1"
+                name="delivery-option-${matchingProduct.id}"
                 />
                 <div>
                 <div class="delivery-option-date">Monday, June 13</div>
